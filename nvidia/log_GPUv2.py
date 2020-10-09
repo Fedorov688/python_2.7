09613cb4aeafd38e4667d8b297c8440bae29883c
sad_ub <fedorov688@gmail.com>
2020-10-09 12:35:36 +0300

    Update log_GPUv2

--- nvidia/log_GPUv2.py
+++ nvidia/log_GPUv2.py
@@ -3,8 +3,6 @@
 import commands
 import time
 from datetime import datetime
-from string import maketrans
-
 
 numbersGPU = 1
 name_of_GPU = "1e82"  # or TITAN or 1d81 or 1080 or 1b80 or Tesla or another shit
@@ -13,6 +11,7 @@ shet_circle = 30000  # кол-во замеров всего
 timeout = 3  # пауза между замерами, сек
 POWEROFF = False  # Выключить сервер по окончанию? True or False
 PEAK_VALUE = [95, 2000, 280, 95]  # [Temperature, Graphics clock, Power Draw, Fan Speed]
+CHECK_CPU_T = False
 
 
 def poweroff():
@@ -22,13 +21,14 @@ def poweroff():
 
 def write_error_message(message):
     """Запись сообщения об ошибке с описанием в общую папку"""
-    commands.getoutput("mkdir " + str(dir[:]) + "/error_message")
-    commands.getoutput("mkdir " + str(dir[:]) + "/error_message/" + str(name_host[:]))
-    commands.getoutput("mkdir " + str(dir[:]) + "/error_message/" + str(name_host[:]) + "/" + time_to_start)
-    t = open(str(dir[:]) + "/error_message/" + str(name_host[:]) + "/" + time_to_start + "/" + "/errors.txt", "a")
+    commands.getoutput("mkdir " + str(current_dir[:]) + "/error_message")
+    commands.getoutput("mkdir " + str(current_dir[:]) + "/error_message/" + str(name_host[:]))
+    commands.getoutput("mkdir " + str(current_dir[:]) + "/error_message/" + str(name_host[:]) + "/" + time_to_start)
+    t = open(str(current_dir[:]) + "/error_message/" + str(name_host[:]) + "/" +
+             time_to_start + "/" + "/errors.txt", "a")
     t.write(message + '\n')
     t.close()
-    c = open(str(dir[:]) + "/error_message/" + "common_errors.log", "a")
+    c = open(str(current_dir[:]) + "/error_message/" + "common_errors.log", "a")
     c.write("Ошибка на модуле {}. Время старта теста {}. Сообщение: \n{}\n".format(
         str(name_host[:]), time_to_start, message))
     c.close()
@@ -56,58 +56,34 @@ def check_value(value, name_of_value, number_of_value, number_gpu):
     global PEAK_VALUE
     if float(value) > float(PEAK_VALUE[number_of_value]):
         print("GPU {} is Freak! {} > {}. (Error! Stop it NOW!!!)".format(number_gpu,
-                                                                         name_of_value, PEAK_VALUE[number_of_value]))
+                                                                         name_of_value,
+                                                                         PEAK_VALUE[number_of_value]))
         messages = "GPU {} is Freak! {} > {}. (Error! Stop it NOW!!!)".format(number_gpu,
-                                                                              name_of_value, PEAK_VALUE[number_of_value])
+                                                                              name_of_value,
+                                                                              PEAK_VALUE[number_of_value])
         write_error_message(message=messages)
         poweroff()
         exit(0)
 
 
-def data(mas3, key_gpu):
-    """работа с массивом mas3(вирт копией среза tmp.txt)"""
-    # значение температуры Ядра ГУ
-    tm_loc = mas3.index('GPU Current Temp')
-    tm_value = mas3[tm_loc + 30:tm_loc + 33]
-    # print(tm_value)
-    tm_clear_value = clear_value(tm_value)
-    check_value(value=tm_clear_value, name_of_value='Temperature GPU', number_of_value=0, number_gpu=key_gpu)
-    # записываем в log значение температуры Ядра ГУ
-    # writers_data(tm, key_gpu, tm_value, wr_mode='a')
-
-    # значение частоты Ядра ГУ
-    cl_loc = mas3.index('Graphics')
-    cl_value = mas3[cl_loc + 29:cl_loc + 35]
-    # print(cl_value)
-    # записываем в log значение частоты Ядра ГУ
-    # writers_data(cl, key_gpu, cl_value, wr_mode='a')
-    cl_clear_value = clear_value(cl_value)
-    check_value(value=cl_clear_value, name_of_value='Graphics clock', number_of_value=1, number_gpu=key_gpu)
-
-    # значение энергопотребления ГУ
-    wt_loc = mas3.index('Power Draw')
-    wt_value = mas3[wt_loc + 29:wt_loc + 36]
-    # print(wt_value)
-    wt_clear_value = clear_value(wt_value)
-    check_value(value=wt_clear_value, name_of_value='Power Draw', number_of_value=2, number_gpu=key_gpu)
-
-    # значение Fan Speed ГУ
-    fn_loc = mas3.index('Fan Speed')
-    fn_value = mas3[fn_loc + 34:fn_loc + 37]
-    # print(fn_value)
-    fn_clear_value = clear_value(fn_value)
-    check_value(value=fn_clear_value, name_of_value='Fan Speed', number_of_value=3, number_gpu=key_gpu)
-
-    # записываем в log значение энергопотребления ГУ
-    # writers_data(wt, key_gpu, wt_value, wr_mode='a')
-    result1 = tm_value + ', ' + cl_value + ', ' + wt_value + ', ' + fn_value + '; '
-    writers_data(res, key_gpu, result1, wr_mode='a')
-    # print(result1)
+def data(gpu_data, key_gpu):
+    """работа с массивом gpu_data(вирт копией среза tmp.txt)"""
+    list_name_index = ['GPU Current Temp', 'Graphics', 'Power Draw', 'Fan Speed']
+    list_index_value = [4, 2, 3, 3]
+    list_name_of_value = ['Temperature GPU', 'Graphics clock', 'Power Draw', 'Fan Speed']
+    tmp = ""
+    for i in range(len(list_name_index)):
+        loc = gpu_data.index(list_name_index[i])
+        value = clear_value(gpu_data[loc:loc + 50].split()[list_index_value[i]])
+        check_value(value=value, name_of_value=list_name_of_value[i], number_of_value=i, number_gpu=key_gpu)
+        tmp += value + ', '
+    tmp += '; '
+    writers_data(param_name="res", key=key_gpu, value=tmp, wr_mode='a')
 
 
 def clear_value(value):
     """Избавление от неугодных"""
-    useful_char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ","]
+    useful_char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", ":", ";", " "]
     tmp = ""
     for word in value:
         if word in useful_char:
@@ -115,19 +91,18 @@ def clear_value(value):
     return tmp
 
 
-def writers_data(param_name, key_gpu, value, wr_mode):
+def writers_data(param_name, key, value, wr_mode):
     """Запись входных параметров по абсолютному адресу с созданием названия"""
     print(clear_value(value))
-    t = open(str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/" + param_name(key_gpu), wr_mode)
+    t = open(str(current_dir[:]) + "/" + str(name_host[:]) + "/" +
+             time_to_start + "/" + str(param_name) + str(key) + ".txt", wr_mode)
     t.write(clear_value(value) + '\n')
     t.close()
 
 
 def init_gpu(proc2, key_gpu):
     """Создание файлов для записи логов, обозначение разъема ГУ"""
-    mas4 = [res, tm, cl, wt]
-    for i in range(1):
-        writers_data(mas4[i], key_gpu, proc2, wr_mode='w')
+    writers_data(param_name="tmp", key=key_gpu, value=proc2, wr_mode='w')
 
 
 def serial_gpu(mas3, key_gpu):
@@ -135,38 +110,22 @@ def serial_gpu(mas3, key_gpu):
     sn_loc = mas3.index('Serial Number')
     sn_value = mas3[sn_loc + 34:sn_loc + 48]
     print(sn_value)
-    writers_data(param_name=res, key_gpu=key_gpu, value=sn_value, wr_mode='a')
+    writers_data(param_name="serial", key=key_gpu, value=sn_value, wr_mode='a')
 
 
 def amb_temperature():
     """Входная температура воздуха у сервера"""
     amb_loc = commands.getoutput('ipmitool sdr type temperature | grep Amb')
     amb_value = amb_loc[38:40]
-    writers_data(param_name=amb, key_gpu=0, value=amb_value, wr_mode='a')
+    writers_data(param_name="amb", key=0, value=amb_value, wr_mode='a')
 
 
-# Генерция названия файлов для log'ов
-def tm(key_gpu):
-    return "temp" + str(key_gpu) + ".txt"
-def cl(key_gpu):
-    return "cloc" + str(key_gpu) + ".txt"
-def wt(key_gpu):
-    return "watt" + str(key_gpu) + ".txt"
-def res(key_gpu):
-    return "res" + str(key_gpu) + ".txt"
-def cpu(key_cpu):
-    return "cpu" + str(key_cpu) + ".txt"
-def amb(key_cpu):
-    return "amb" + str(key_cpu) + ".txt"
-
-
-def initial_GPU():
+def initial_gpu():
     """Создание временного файла, с содержанием данных о ГУ"""
-    # commands.getoutput("nvidia-smi -a >" + str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt")
-    commands.getoutput('cat /media/sad_ub/32GB/soft/nvidia_log/3080.txt >' + str(dir[:]) + "/" + str(
-        name_host[:]) + "/" + time_to_start + "/tmp.txt")
+    commands.getoutput("nvidia-smi -a >" +
+                       str(current_dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt")
 
-    mas0 = commands.getoutput("cat " + str(dir[:]) + "/" + str(name_host[:]) +
+    mas0 = commands.getoutput("cat " + str(current_dir[:]) + "/" + str(name_host[:]) +
                               "/" + time_to_start + "/tmp.txt | grep 'GPU 0000'")
     mas1 = [0, 21, 42, 63, 84, 105, 126, 147]
     mas11 = [20, 41, 62, 83, 104, 125, 146, 167]
@@ -175,8 +134,8 @@ def initial_GPU():
         print(key_gpu)
         proc2 = mas0[int(mas1[key_gpu]):int(mas11[key_gpu])]
         mas3 = commands.getoutput(
-            "cat " + str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt | grep '" + str(
-                proc2) + "' --after-context=144")
+            "cat " + str(current_dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt | grep '" + str(
+                proc2) + "' --after-context=177")
         init_gpu(proc2, key_gpu)
         serial_gpu(mas3, key_gpu)
 
@@ -185,30 +144,30 @@ def monit():
     global name_of_GPU
     proc1 = commands.getoutput('lspci | grep ' + name_of_GPU + ' |wc -l')
     proc1_1 = commands.getoutput('ipmitool sdr type temperature | grep CPU | wc -l')  # for ipmitools
-    mas0 = commands.getoutput("cat " + str(dir[:]) + "/" + str(name_host[:]) +
+    mas0 = commands.getoutput("cat " + str(current_dir[:]) + "/" + str(name_host[:]) +
                               "/" + time_to_start + "/tmp.txt | grep 'GPU 0000'")
     mas1 = [0, 21, 42, 63, 84, 105, 126, 147]
     mas11 = [20, 41, 62, 83, 104, 125, 146, 167]
     shet_1 = 0
     while shet_1 < shet_circle:
-        # commands.getoutput('nvidia-smi -a >' + str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt")
-        # "cat /media/sad_ub/32GB/soft/nvidia_log/3080.txt"
-        commands.getoutput('cat /media/sad_ub/32GB/soft/nvidia_log/3080.txt >' + str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt")
-
+        commands.getoutput('nvidia-smi -a >' +
+                           str(current_dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + "/tmp.txt")
         # Проход по каждой ГУ, создание среза с tmp.txt, обращение к функции data для записи данных
         for key_gpu in range(int(proc1)):
             proc2 = mas0[int(mas1[key_gpu]):int(mas11[key_gpu])]
-            mas3 = commands.getoutput("cat " + str(dir[:]) + "/" + str(name_host[:]) +
-                                      "/" + time_to_start + "/tmp.txt | grep '" + str(proc2) + "' --after-context=144")
-            data(mas3, key_gpu)
-        # mas3_3 = commands.getoutput('sensors | grep Physical') # for sensors
-        # mas3_3 = commands.getoutput('ipmitool sdr type temperature | grep CPU')  # for ipmitools
-        # for key_cpu in range(int(proc1_1)):
-        #     mas1_1 = [37, 88]  # [16, 79] # for sensors
-        #     mas11_1 = [40, 91]  # [20, 83] # for sensors
-        #     cpu_value = mas3_3[int(mas1_1[key_cpu]):int(mas11_1[key_cpu])]
-        #     # print(cpu_value)
-        #     writers_data(cpu, key_cpu, cpu_value, wr_mode='a')
+            mas3 = commands.getoutput("cat " + str(current_dir[:]) + "/" + str(name_host[:]) +
+                                      "/" + time_to_start + "/tmp.txt | grep '" + str(proc2) + "' --after-context=170")
+            data(gpu_data=mas3, key_gpu=key_gpu)
+        if CHECK_CPU_T:
+            # mas3_3 = commands.getoutput('sensors | grep Physical') # for sensors
+            mas3_3 = commands.getoutput('ipmitool sdr type temperature | grep CPU')  # for ipmitools
+            for key_cpu in range(int(proc1_1)):
+                mas1_1 = [37, 88]  # [16, 79] # for sensors
+                mas11_1 = [40, 91]  # [20, 83] # for sensors
+                cpu_value = mas3_3[int(mas1_1[key_cpu]):int(mas11_1[key_cpu])]
+                # print(cpu_value)
+                writers_data(param_name="cpu", key=key_cpu, value=cpu_value, wr_mode='a')
+
         print(datetime.now())
         time.sleep(timeout)
         shet_1 += 1
@@ -219,7 +178,8 @@ def association_txt():
     common_tmp = []
     schet = 0  # номер строки
     for i in range(k):
-        tmp = open(str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + '/' + 'res' + str(i) + '.txt', 'r')
+        tmp = open(str(current_dir[:]) + "/" + str(name_host[:]) + "/" +
+                   time_to_start + '/' + 'res' + str(i) + '.txt', 'r')
         common_tmp.append(tmp.readlines())
         tmp.close()
     print(common_tmp)
@@ -239,18 +199,18 @@ def association_txt():
             schet = shet_circle + 1
         schet += 1
     print(common_result)
-    result = open(str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + '/' + 'result.txt', 'w')
+    result = open(str(current_dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start + '/' + 'result.txt', 'w')
     result.write(common_result)
 
 
-dir = commands.getoutput('pwd')
+current_dir = commands.getoutput('pwd')
 name_host = commands.getoutput('uname -n')
 time_to_start = datetime.strftime(datetime.now(), "%Y_%m_%d_%H_%M_%S")
-make_host_dir = commands.getoutput('mkdir ' + str(dir[:]) + "/" + str(name_host[:]))
-make_dir = commands.getoutput('mkdir ' + str(dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start)
-initial_GPU()
-# check_gpu(default_numbersgpu=numbersGPU, pci_numbersgpu=k)
-print(dir[:])
+make_host_dir = commands.getoutput('mkdir ' + str(current_dir[:]) + "/" + str(name_host[:]))
+make_dir = commands.getoutput('mkdir ' + str(current_dir[:]) + "/" + str(name_host[:]) + "/" + time_to_start)
+initial_gpu()
+check_gpu(default_numbersgpu=numbersGPU, pci_numbersgpu=k)
+print(current_dir[:])
 print(name_host[:])
 try:
     amb_temperature()
